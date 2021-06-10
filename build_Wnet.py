7257308c9f61d7978cfa999ddc010a4c6cdd46e6
JereMonk <jeremy@monkvision.ai>
2021-06-10 16:44:41 +0000

    add tf func deco

--- build_Wnet.py
+++ build_Wnet.py
@@ -31,38 +31,38 @@ class Wnet(tf.keras.Model):
       output = self.decoder(self.encoder(inputs))
       return output
 
+    @tf.function
+    def test_step(self,batch_data):
+        image= batch_data
+
+        result_encoder = self.encoder(image)
+        result_decoder = self.decoder(result_encoder)
+        loss_decoder = self.loss_fn_reconstruction(image,result_decoder)
+        loss_encoder = self.loss_fn_segmentation(image,result_encoder,self.neighbour_filter)
+
+        return {
+            "loss_encoder": loss_encoder,
+            "loss_decoder": loss_decoder,
+        }
+
+    @tf.function
     def train_step(self, batch_data):
-        
         image = batch_data
-
-        print(image.shape)
-
+        
         with tf.GradientTape(persistent=True) as tape:
           
           result_encoder = self.encoder(image)
           result_decoder = self.decoder(result_encoder)
-
-          print(result_encoder.shape)
-          print(result_decoder.shape)
-
           loss_decoder = self.loss_fn_reconstruction(image,result_decoder)
-          print(loss_decoder)
-
           loss_encoder = self.loss_fn_segmentation(image,result_encoder,self.neighbour_filter)
-
-          print(loss_encoder)
+   
           
           
-
-    
         grads_encoder_1 = tape.gradient(loss_encoder, self.encoder.trainable_variables)
-
         grads_encoder_2 = tape.gradient(loss_decoder, self.encoder.trainable_variables)
-
         grads_decoder = tape.gradient(loss_decoder, self.decoder.trainable_variables)
 
    
-
         self.optimizer.apply_gradients(
             zip(grads_encoder_1, self.encoder.trainable_variables)
         )
@@ -73,8 +73,6 @@ class Wnet(tf.keras.Model):
         self.optimizer.apply_gradients(
             zip(grads_decoder, self.decoder.trainable_variables)
         )
-
-
 
         return {
             "loss_encoder": loss_encoder,
