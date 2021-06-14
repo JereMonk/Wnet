93578192b0664a4fcecb6d69084e68e9ca719213
JereMonk <jeremy@monkvision.ai>
2021-06-14 07:29:16 +0000

    ?

--- build_Unet.py
+++ build_Unet.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 def build_Unet(K,stages,filters,type):
 
-  k_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.02, seed=None)
+  #k_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.02, seed=None)
 
   concats=[]
   
@@ -13,11 +13,11 @@ def build_Unet(K,stages,filters,type):
 
   x = inputs
   for i in stages:
-    x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same',kernel_initializer=k_initializer)(x)
+    x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x= tf.keras.layers.ReLU()(x)
 
-    x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same',kernel_initializer=k_initializer)(x)
+    x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x= tf.keras.layers.ReLU()(x)
 
@@ -32,11 +32,11 @@ def build_Unet(K,stages,filters,type):
     
 
 
-  x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same',kernel_initializer=k_initializer)(x)
+  x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same')(x)
   x = tf.keras.layers.BatchNormalization()(x)
   x= tf.keras.layers.ReLU()(x)
 
-  x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same',kernel_initializer=k_initializer)(x)
+  x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same')(x)
   x = tf.keras.layers.BatchNormalization()(x)
   x= tf.keras.layers.ReLU()(x)
 
@@ -44,24 +44,24 @@ def build_Unet(K,stages,filters,type):
 
     filters /=2
 
-    u = tf.keras.layers.Conv2DTranspose(filters=filters,kernel_size=(2,2),strides=(2,2),padding='valid',kernel_initializer=k_initializer)(x)
+    u = tf.keras.layers.Conv2DTranspose(filters=filters,kernel_size=(2,2),strides=(2,2),padding='valid')(x)
     y= concats[-i]
     
     x = tf.keras.layers.Concatenate(axis=3)([y, u])
     
-    x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same',kernel_initializer=k_initializer)(x)
+    x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x= tf.keras.layers.ReLU()(x)
 
-    x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same',kernel_initializer=k_initializer)(x)
+    x= tf.keras.layers.Conv2D(filters=filters,kernel_size=(3,3),strides=(1,1),padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x= tf.keras.layers.ReLU()(x)
 
   if(type == 'encoder'):
-    x = tf.keras.layers.Conv2D(filters=K,kernel_size=(1,1),strides=(1,1),padding='same',kernel_initializer=k_initializer)(x)
+    x = tf.keras.layers.Conv2D(filters=K,kernel_size=(1,1),strides=(1,1),padding='same')(x)
     x = tf.keras.layers.Softmax(axis=-1)(x)
   else:
-    x = tf.keras.layers.Conv2D(filters=3,kernel_size=(1,1),strides=(1,1),padding='same',kernel_initializer=k_initializer)(x)
+    x = tf.keras.layers.Conv2D(filters=3,kernel_size=(1,1),strides=(1,1),padding='same')(x)
     x= tf.keras.layers.Activation('sigmoid')(x)
 
   model = tf.keras.Model(inputs=inputs, outputs=x)
