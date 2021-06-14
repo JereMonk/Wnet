538dfb616501e319bd28b25856f33b57dc6b0230
JereMonk <jeremy@monkvision.ai>
2021-06-14 07:23:48 +0000

    ?

--- ncut_loss.py
+++ ncut_loss.py
@@ -443,10 +443,7 @@ def soft_ncut(image, image_segment, image_weights):
     
     # Soft NCut
     eps = 1e-20
-    print(dis_assoc)
-    print(assoc)
-    print(dis_assoc/assoc)
-    print( tf.reduce_sum((dis_assoc +eps) / (assoc+eps), axis=1))
+
     soft_ncut = tf.cast(num_class, tf.float32) - tf.reduce_sum((dis_assoc +eps) / (assoc+eps), axis=1)
     
     return soft_ncut
